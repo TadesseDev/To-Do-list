f5bfa7bc1f5d0740b9c02bbe811e8ffc73df784e
Tadesse-Alemayehu <email24t@gmail.com>
2022-03-23 00:52:20 +0300

    Udate everything from js file

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './main.css';
-import data from './modules/data';
-import { updateTask } from './modules/methods';
+import data from './modules/data.js';
+import { updateTask } from './modules/methods.js';
 
 const listContainer = document.getElementById('to-do-lists');
 updateTask(listContainer, data);
-console.log(data);
--- src/modules/methods.js
+++ src/modules/methods.js
@@ -1,27 +1,27 @@
-
-// can be used to sort tasks ascending or descending 
+// can be used to sort tasks ascending or descending
 const sortTaskByIndex = (data, mode = 'ascending') => {
-  let tempData = [...data]
-  if (mode === 'ascending')
-    tempData.sort((a, b) => a.index - b.index);
-  else
-    tempData.sort((a, b) => b.index - a.index);
+  const tempData = [...data];
+  if (mode === 'ascending') tempData.sort((a, b) => a.index - b.index);
+  else tempData.sort((a, b) => b.index - a.index);
   return tempData;
-}
+};
 
 // sort and update list of tasks to the place holder
 const updateListOfTasks = (section, data) => {
+
+  section.innerHTML = `<li><span class="title">Today's To Do</span></li>
+                        <li><label for="new-task"></label><input type="text" id="new-task" placeholder="Add your list"></li>`
   data = sortTaskByIndex(data);
   data.forEach((task) => {
     const li = document.createElement('li');
     const input = `<input type="checkbox" id="${task.index}" name="${task.index}" value="${task.index}"></input>`;
-    const label = `<label for="${task.index}">${task.description}</label>`
+    const label = `<label for="${task.index}">${task.description}</label>`;
     li.innerHTML = input + label;
     section.appendChild(li);
   });
   const li = document.createElement('li');
-  li.innerHTML = "<p>Clear all completed</p>";
+  li.innerHTML = '<p>Clear all completed</p>';
   section.appendChild(li);
 };
 
-export { updateListOfTasks as updateTask };
+export { updateListOfTasks as updateTask, sortTaskByIndex };
