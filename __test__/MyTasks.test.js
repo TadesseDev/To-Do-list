60ba047103385416c89f68a66182c966cc92c107
Tadesse-Alemayehu <email24t@gmail.com>
2022-03-31 16:42:35 +0300

    add test inviroment to the js config file

--- __test__/MyTasks.test.js
+++ __test__/MyTasks.test.js
@@ -1,19 +1,14 @@
 import MyTasks from "../src/modules/MyTasks";
-const localStorageMock = {
-  data: {},
-  getItem: jest.fn((name) => localStorageMock.data[name]),
-  setItem: jest.fn((name, value) => {
-    localStorageMock.data[name] = value;
-  }),
-  clear: jest.fn()
-};
-global.localStorage = localStorageMock;
+
 const newTask1 = new MyTasks('task one');
 const newTask2 = new MyTasks('task two');
 const newTask3 = new MyTasks('task three');
 console.log(MyTasks.tasks);
 test('test adding new element to the local storage and task list', () => {
-  expect(localStorage.setItem).toHaveBeenCalled();
+  // expect(localStorage.setItem).toHaveBeenCalled();
   expect(JSON.parse(localStorage.getItem('taskList')).length).toEqual(3);
   expect(MyTasks.tasks.length).toBe(3);
 });
+test('add and remove from and to the DOM', () => {
+
+})
