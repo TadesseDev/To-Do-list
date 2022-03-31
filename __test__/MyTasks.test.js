d01c6ce597e9532efc8c7e6e93cb674cdff3c783
Tadesse-Alemayehu <email24t@gmail.com>
2022-03-31 17:30:05 +0300

    configure jest test envionment

--- __test__/MyTasks.test.js
+++ __test__/MyTasks.test.js
@@ -1,19 +1,7 @@
 import MyTasks from "../src/modules/MyTasks";
-const localStorageMock = {
-  data: {},
-  getItem: jest.fn((name) => localStorageMock.data[name]),
-  setItem: jest.fn((name, value) => {
-    localStorageMock.data[name] = value;
-  }),
-  clear: jest.fn()
-};
-global.localStorage = localStorageMock;
+
 const newTask1 = new MyTasks('task one');
 const newTask2 = new MyTasks('task two');
 const newTask3 = new MyTasks('task three');
-console.log(MyTasks.tasks);
-test('test adding new element to the local storage and task list', () => {
-  expect(localStorage.setItem).toHaveBeenCalled();
-  expect(JSON.parse(localStorage.getItem('taskList')).length).toEqual(3);
-  expect(MyTasks.tasks.length).toBe(3);
-});
+document.body.innerHTML = `<ul id="to-do-lists"></ul>`;
+const section = document.getElementById('to-do-lists');
